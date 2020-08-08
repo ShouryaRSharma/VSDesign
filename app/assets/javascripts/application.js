277582f1f8c093e2cf83461850952ae00377e09d
shourya <ss02184@surrey.ac.uk>
2020-08-08 01:33:43 +0100

    VSDesignWebsite

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,26 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require bootstrap/dist/js/bootstrap
+//= require smooth-scroll/dist/smooth-scroll.js
+//= require_tree .
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 500,
+    speedAsDuration: true,
+    easing: 'easeInOutCubic',
+    updateURL: true,
+    popstate: true,
+});
